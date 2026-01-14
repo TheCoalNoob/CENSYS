3cdb67ce55f09e4b9e36c3066183e93e1413f0c2
TheCoalNoob <adrianevincentjugo@gmail.com>
2026-01-14 09:52:03 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,10 @@
 var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "YOUR_FIREBASE_API_KEY",
   authDomain: "YOUR_PROJECT.firebaseapp.com",
   databaseURL: "https://YOUR_PROJECT.firebaseio.com",
   projectId: "YOUR_PROJECT",
 };
-
 firebase.initializeApp(firebaseConfig);
+
+var auth = firebase.auth();
+var db = firebase.database();
